b4b3943678a3417b8e36a89546667d70ae2513d8
the-crypto-trade <185878696+the-crypto-trade@users.noreply.github.com>
2025-02-07 12:51:26 -0800

    ci

--- format.sh
+++ format.sh
@@ -6,15 +6,15 @@ set -o pipefail
 echo "Running format.sh..."
 
 echo "Running black..."
-find . -type f -not -path "*/.venv*/*" -not -path "*/__pycache__/*" -not -path "*/build/*" -name "*.py" -exec black --line-length 160 {} \+
+find . -type f \( -path "./src/*" -o -path "./tests/*" -o -path "./examples/*" \) -not -path "*/__pycache__/*" -not -path "*/crypto_trade.egg-info/*" -name "*.py" -print -exec black --line-length 160 {} \+
 
 echo "Running isort..."
-find . -type f -not -path "*/.venv*/*" -not -path "*/__pycache__/*" -not -path "*/build/*" -name "*.py" -exec isort --profile black {} \+
+find . -type f \( -path "./src/*" -o -path "./tests/*" -o -path "./examples/*" \) -not -path "*/__pycache__/*" -not -path "*/crypto_trade.egg-info/*" -name "*.py" -print -exec isort --profile black {} \+
 
 echo "Running autoflake..."
-find . -type f -not -path "*/.venv*/*" -not -path "*/__pycache__/*" -not -path "*/build/*" -name "*.py" -exec autoflake --in-place --remove-all-unused-imports --remove-unused-variables {} \+
+find . -type f \( -path "./src/*" -o -path "./tests/*" -o -path "./examples/*" \) -not -path "*/__pycache__/*" -not -path "*/crypto_trade.egg-info/*" -name "*.py" -print -exec autoflake --in-place --remove-all-unused-imports --remove-unused-variables {} \+
 
 echo "Running autopep8..."
-find . -type f -not -path "*/.venv*/*" -not -path "*/__pycache__/*" -not -path "*/build/*" -name "*.py" -exec autopep8 --in-place --max-line-length 160  --select=E231 {} \+
+find . -type f \( -path "./src/*" -o -path "./tests/*" -o -path "./examples/*" \) -not -path "*/__pycache__/*" -not -path "*/crypto_trade.egg-info/*" -name "*.py" -print -exec autopep8 --in-place --max-line-length 160  --select=E231 {} \+
 
 echo "Done."
--- lint.sh
+++ lint.sh
@@ -5,12 +5,12 @@ set -o pipefail
 echo "Running lint.sh..."
 
 echo "Running flake8..."
-find . -type f -not -path "*/.venv*/*" -not -path "*/__pycache__/*" -not -path "*/build/*" -name "*.py" -not -name "bybit.py" -exec flake8 --max-line-length 160 --extend-ignore E203 {} \+
+find . -type f \( -path "./src/*" -o -path "./tests/*" -o -path "./examples/*" \) -not -path "*/__pycache__/*" -not -path "*/crypto_trade.egg-info/*" -name "*.py" -not -name "bybit.py" -print -exec flake8 --max-line-length 160 --extend-ignore E203 {} \+
 
 echo "Running mypy..."
-find . -type f -not -path "*/.venv*/*" -not -path "*/__pycache__/*" -not -path "*/build/*" -name "*.py" -not -name "bybit.py" -exec mypy {} \+
+find . -type f \( -path "./src/*" -o -path "./tests/*" -o -path "./examples/*" \) -not -path "*/__pycache__/*" -not -path "*/crypto_trade.egg-info/*" -name "*.py" -not -name "bybit.py" -print -exec mypy {} \+
 
 echo "Running pylint..."
-find . -type f -not -path "*/.venv*/*" -not -path "*/__pycache__/*" -not -path "*/build/*" -name "*.py" -not -name "bybit.py" -exec pylint --errors-only {} \+
+find . -type f \( -path "./src/*" -o -path "./tests/*" -o -path "./examples/*" \) -not -path "*/__pycache__/*" -not -path "*/crypto_trade.egg-info/*" -name "*.py" -not -name "bybit.py" -print -exec pylint --errors-only {} \+
 
 echo "Done."
